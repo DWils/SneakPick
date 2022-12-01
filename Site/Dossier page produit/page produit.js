481216bba1648d779f2976dcd79ab4095c218889
jeremy.bondaz@icloud.com <jeremy.bondaz@icloud.com>
2022-12-01 12:19:43 +0100

    ajout de toutes les pages produits et du script et changement dans le répertoire du site pour mieux organiser la chose -Jérémy

--- Site/Dossier page produit/page produit.js
+++ Site/Dossier page produit/page produit.js
@@ -2,22 +2,22 @@ const sliderMainImage = document.getElementById("product-main-image"); //product
 const sliderImageList = document.getElementsByClassName("image-list"); // image thumblian group selection
 console.log(sliderImageList);
 
-sliderImageList[0].onclick = function(){
+sliderImageList[0].onclick = function () {
     sliderMainImage.src = sliderImageList[0].src;
     console.log(sliderMainImage.src);
 };
 
-sliderImageList[1].onclick = function(){
+sliderImageList[1].onclick = function () {
     sliderMainImage.src = sliderImageList[1].src;
     console.log(sliderMainImage.src);
 };
 
-sliderImageList[2].onclick = function(){
+sliderImageList[2].onclick = function () {
     sliderMainImage.src = sliderImageList[2].src;
     console.log(sliderMainImage.src);
 };
 
-sliderImageList[3].onclick = function(){
+sliderImageList[3].onclick = function () {
     sliderMainImage.src = sliderImageList[3].src;
     console.log(sliderMainImage.src);
 };
